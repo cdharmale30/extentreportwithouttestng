04a8105cd0d783181c994c96107b9f8e2f89a448
cdharmale30 <36369070+cdharmale30@users.noreply.github.com>
2018-11-29 04:33:46 -0800

    Extent

---
+++ src/com/test/extentreport/Ab.java
@@ -0,0 +1,15 @@
+package com.test.extentreport;
+
+import org.testng.annotations.Test;
+
+public class Ab {
+
+	
+public static void main(String[] args) {
+	String current = System.getProperty("user.dir");
+    System.out.println("Current working directory in Java : " + current);
+		}
+	
+}
+	
+    
